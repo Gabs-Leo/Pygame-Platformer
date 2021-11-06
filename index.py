da622de8a5b6371824f1774c044f5c3d8f6a5de5
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-11-06 02:18:05 -0300

    :construction: Adicionando Surfaces

--- index.py
+++ index.py
@@ -3,9 +3,23 @@ import sys
 
 pygame.init()
 SCREEN_SIZE = (1280, 720)
-FPS_LIMIT = 75
+FPS_LIMIT = 60
+pygame.display.set_caption("Game #1")
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+
+#Main Surface
 screen = pygame.display.set_mode((SCREEN_SIZE))
 clock = pygame.time.Clock()
+testFont = pygame.font.Font("src/fonts/Pixelmania.ttf", 48)
+
+#Extra Surfaces
+testSurface = pygame.Surface((128, 128))
+testSurface.fill("cyan")
+
+imageSurface = pygame.image.load("src/player.png")
+
+textSurface = testFont.render("MY TEXT", False, "white")
 
 while True:
     for event in pygame.event.get():
@@ -14,5 +28,9 @@ while True:
             sys.exit()
 
     screen.fill('black')
+    screen.blit(testSurface, (SCREEN_SIZE[0] / 2 - 64, SCREEN_SIZE[1] / 2 - 64))
+    screen.blit(imageSurface, (SCREEN_SIZE[0] / 2 - 64, SCREEN_SIZE[1] / 2 - 64))
+    screen.blit(textSurface, (0,0))
+
     pygame.display.update()
     clock.tick(FPS_LIMIT)
